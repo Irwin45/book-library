e27c6d618989c28c59a3b14600a31448905828ad
Wangweibing <lpstudio2006@gmail.com>
2024-12-15 23:31:28 +0300

    Added createAsyncThunk

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { addBook, thunkFunction } from '../../redux/slices/booksSlice';
+import { addBook, fetchBook } from '../../redux/slices/booksSlice';
 // import { addBook } from '../../redux/books/actionCreaters';
 import createBookWithID from '../../utils/createBookWithID';
 import booksData from '../../data/books.json';
@@ -20,7 +20,7 @@ const BookForm = () => {
   };
 
   const handleAddRandomBookViaAPI = () => {
-    dispatch(thunkFunction);
+    dispatch(fetchBook());
     //старый варината. Асинк функция сразу в элементе.
     // try {
     //   const res = await axios.get('http://localhost:4000/random-book');
--- frontend/src/redux/slices/booksSlice.js
+++ frontend/src/redux/slices/booksSlice.js
@@ -1,8 +1,19 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import createBookWithID from '../../utils/createBookWithID';
 
 const initialState = [];
+
+export const fetchBook = createAsyncThunk(
+  //как будет называться действие
+  'books/fetchBook',
+  //тут асинхронная функция
+  async () => {
+    const res = await axios.get('http://localhost:5000/random-book');
+    // console.log(res.data);
+    return res.data;
+  },
+);
 
 const booksSlice = createSlice({
   name: 'books',
@@ -38,26 +49,40 @@ const booksSlice = createSlice({
       // );
     },
   },
+
+  extraReducers: (builder) => {
+    builder.addCase(
+      //если асинх функция фулфилд, то далее выполняется функция во втором аргументе
+      fetchBook.fulfilled,
+      //а это функция уже просто Редюсер, который добавляет все состояние
+      (state, action) => {
+        if (action.payload.title && action.payload.author) {
+          state.push(createBookWithID(action.payload, 'API'));
+        }
+      },
+    );
+  },
 });
 
 export const { addBook, deleteBook, toggleFavorite } = booksSlice.actions;
 
 //размещаем эту функцию под деструктуризацией объекта их которого мы берем акшин для диспатча - аддбук
-export const thunkFunction = async (dispatch, getState) => {
-  // console.log(getState());
-  // async action
-  try {
-    const res = await axios.get('http://localhost:4000/random-book');
-    // console.log(res);
-    // res.data && res.data.title && res.data.author;
-    if (res?.data?.title && res?.data?.author) {
-      dispatch(addBook(createBookWithID(res.data, 'API')));
-    }
-  } catch (error) {
-    console.log('Error faetchin random book', error);
-  }
-  // console.log(getState());
-};
+// export const thunkFunction = async (dispatch, getState) => {
+//   // можно всегда получит текущее
+//   // console.log(getState());
+//   // async action
+//   try {
+//     const res = await axios.get('http://localhost:4000/random-book');
+//     // console.log(res);
+//     // res.data && res.data.title && res.data.author;
+//     if (res?.data?.title && res?.data?.author) {
+//       dispatch(addBook(createBookWithID(res.data, 'API')));
+//     }
+//   } catch (error) {
+//     console.log('Error faetchin random book', error);
+//   }
+//   // console.log(getState());
+// };
 
 export const selectBooks = (state) => state.books;
 
